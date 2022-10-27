ac555159ffc1b2b06a2042730a57d6e31d3d76e0
okuzawats <okuzawats@gmail.com>
2022-10-27 21:40:05 +0900

    project root build.gradle to build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,5 @@
+plugins {
+  id("com.android.application") version libs.versions.android.gradle.plugin apply false
+  id("com.android.library") version libs.versions.android.gradle.plugin apply false
+  id("org.jetbrains.kotlin.android") version libs.versions.kotlin apply false
+}
