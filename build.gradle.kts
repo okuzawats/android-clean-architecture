821455a5aa9500cde5a4534deb5995fa906f3455
okuzawats <okuzawats@gmail.com>
2022-10-29 16:16:44 +0900

    Introduce Retrofit (#9)
    * add some libraries to use retrofit
    
    * use hilt plugin by id

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,7 @@
 plugins {
   id("com.android.application") version libs.versions.android.gradle.plugin apply false
   id("com.android.library") version libs.versions.android.gradle.plugin apply false
-  id("org.jetbrains.kotlin.android") version libs.versions.kotlin apply false
-}
-
-buildscript {
-  dependencies {
-    classpath(libs.hilt.android.gradle.plugin)
-  }
+  id("org.jetbrains.kotlin.android") version libs.versions.kotlin.core apply false
+  id("org.jetbrains.kotlin.plugin.serialization") version libs.versions.kotlin.core apply false
+  id("com.google.dagger.hilt.android") version libs.versions.hilt apply false
 }
