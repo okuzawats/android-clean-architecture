2f3cd45f32ec88f9655df16054f124276363dbc1
okuzawats <okuzawats@gmail.com>
2022-10-29 15:46:02 +0900

    rename app/build.gradle to kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,14 +6,14 @@ plugins {
 }
 
 android {
-  namespace("com.okuzawats.cleanarchitecture")
-  compileSdk = libs.versions.android.compileSdk.get().toInteger()
+  namespace = "com.okuzawats.cleanarchitecture"
+  compileSdk = libs.versions.android.compileSdk.get().toInt()
 
   defaultConfig {
     applicationId = "com.okuzawats.cleanarchitecture"
-    minSdk = libs.versions.android.minSdk.get().toInteger()
-    targetSdk = libs.versions.android.targetSdk.get().toInteger()
-    versionCode = libs.versions.app.versionCode.get().toInteger()
+    minSdk = libs.versions.android.minSdk.get().toInt()
+    targetSdk = libs.versions.android.targetSdk.get().toInt()
+    versionCode = libs.versions.app.versionCode.get().toInt()
     versionName = libs.versions.app.versionName.get()
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
@@ -24,7 +24,7 @@ android {
 
   buildTypes {
     getByName("release") {
-      minifyEnabled = false
+      isMinifyEnabled = false
       proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
     }
   }
