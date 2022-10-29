d27475c25296a585aa59c2588eaa14b33b521121
okuzawats <okuzawats@gmail.com>
2022-10-29 16:14:22 +0900

    use hilt plugin by id

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,5 @@ plugins {
   id("com.android.library") version libs.versions.android.gradle.plugin apply false
   id("org.jetbrains.kotlin.android") version libs.versions.kotlin.core apply false
   id("org.jetbrains.kotlin.plugin.serialization") version libs.versions.kotlin.core apply false
-}
-
-buildscript {
-  dependencies {
-    classpath(libs.hilt.android.gradle.plugin)
-  }
+  id("com.google.dagger.hilt.android") version libs.versions.hilt apply false
 }
