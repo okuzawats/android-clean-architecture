61514d3b8ac6fe2cac4821508eb3d9a51657ad87
okuzawats <okuzawats@gmail.com>
2022-10-30 20:49:11 +0900

    remove unnecessory settings

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -3,7 +3,6 @@ plugins {
   id("org.jetbrains.kotlin.android")
   id("com.google.dagger.hilt.android")
   id("kotlin-kapt")
-  id("kotlinx-serialization")
 }
 
 android {
@@ -14,7 +13,6 @@ android {
     minSdk = libs.versions.android.minSdk.get().toInt()
     targetSdk = libs.versions.android.targetSdk.get().toInt()
 
-    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     consumerProguardFiles("consumer-rules.pro")
   }
 
@@ -31,45 +29,12 @@ android {
   kotlinOptions {
     jvmTarget = JavaVersion.VERSION_11.toString()
   }
-  buildFeatures {
-    compose = true
-  }
-  composeOptions {
-    kotlinCompilerExtensionVersion = libs.versions.compose.ui.get()
-  }
-  packagingOptions {
-    resources {
-      excludes += "/META-INF/{AL2.0,LGPL2.1}"
-    }
-  }
 }
 
 dependencies {
-  implementation(libs.androidx.core.ktx)
-  implementation(libs.androidx.lifecycle.runtime.ktx)
-  implementation(libs.androidx.lifecycle.viewmodel.ktx)
-  implementation(libs.androidx.lifecycle.viewmodel.compose)
-  implementation(libs.androidx.activity.ktx)
-  implementation(libs.androidx.activity.compose)
-  implementation(libs.androidx.compose.material)
-  androidTestImplementation(libs.androidx.test.ext.junit)
-  androidTestImplementation(libs.androidx.test.espresso.core)
-
-  implementation(libs.compose.ui)
-  implementation(libs.compose.ui.tooling.preview)
-  debugImplementation(libs.compose.ui.tooling)
-  debugImplementation(libs.compose.ui.test.manifest)
-  androidTestImplementation(libs.compose.ui.test.junit4)
-
   implementation(libs.hilt.android)
   kapt(libs.hilt.compiler)
-
-  implementation(libs.kotlin.serialization)
-  implementation(libs.retrofit)
-  implementation(libs.retrofit.converter)
-  implementation(libs.okhttp.core)
   implementation(libs.arrow.core)
-
   testImplementation(libs.junit)
 }
 
