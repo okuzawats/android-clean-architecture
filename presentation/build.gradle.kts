a94538f2c43f163f964abb5aebd1ac41f19fe21f
okuzawats <okuzawats@gmail.com>
2022-11-01 21:59:32 +0900

    Create presentation and ui modules

---
+++ presentation/build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+  id("com.android.library")
+  id("org.jetbrains.kotlin.android")
+  id("com.google.dagger.hilt.android")
+  id("kotlin-kapt")
+}
+
+android {
+  namespace = "com.okuzawats.cleanarchitecture.presentation"
+  compileSdk = libs.versions.android.compileSdk.get().toInt()
+
+  defaultConfig {
+    minSdk = libs.versions.android.minSdk.get().toInt()
+    targetSdk = libs.versions.android.targetSdk.get().toInt()
+
+    consumerProguardFiles("consumer-rules.pro")
+  }
+
+  buildTypes {
+    getByName("release") {
+      isMinifyEnabled = false
+      proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+    }
+  }
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+  }
+  kotlinOptions {
+    jvmTarget = JavaVersion.VERSION_11.toString()
+  }
+}
+
+dependencies {
+  implementation(project(":domain"))
+  implementation(libs.androidx.lifecycle.viewmodel.ktx)
+  implementation(libs.arrow.core)
+  implementation(libs.hilt.android)
+  kapt(libs.hilt.compiler)
+  testImplementation(libs.junit)
+}
